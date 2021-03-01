add115e186e5aeb9a92e67ddf24c8589088e8187
Glitch (olive-marble-wolfberry) <none>
2021-03-01 14:10:22 +0100

    idfhidbsfhubdgsyufsfs

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const client = new Client({
   restTimeOffset: 0
 });
 
-client.login(TOKEN);
+client.login(process.env.token);
 client.commands = new Collection();
 client.prefix = PREFIX;
 client.queue = new Map();
